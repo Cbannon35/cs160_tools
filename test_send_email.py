ed106a26f11e9e961668ecfcd17a37589154d896
Christopher Bannon <bannon.c.35@berkeley.edu>
2023-07-05 01:10:18 -0700

    more email work

--- test_send_email.py
+++ test_send_email.py
@@ -11,14 +11,49 @@ flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
 creds = flow.run_local_server(port=0)
 
 service = build('gmail', 'v1', credentials=creds)
-message = MIMEText('This is the body of the email')
-message['to'] = 'bannon.c.35@gmail.com'
-message['subject'] = 'Email Subject'
-create_message = {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
 
-try:
-    message = (service.users().messages().send(userId="me", body=create_message).execute())
-    print(F'sent message to {message} Message Id: {message["id"]}')
-except HTTPError as error:
-    print(F'An error occurred: {error}')
-    message = None
+def create_message(sender, to, Cc, subject, message_text):
+    message = MIMEText(message_text)
+    message['to'] = to
+    message['subject'] = subject
+    message['from'] = sender
+    message['Cc'] = Cc
+    return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
+
+def send_message(message):
+    try:
+        message = (service.users().messages().send(userId="me", body=create_message).execute())
+        print(F'sent message to {message} Message Id: {message["id"]}')
+    except HTTPError as error:
+        print(F'An error occurred: {error}')
+        message = None
+
+def email(sender, to, Cc, subject, message_text):
+    message = create_message(sender, to, Cc, subject, message_text)
+    send_message(message)
+
+if __name__ == '__main__':
+    message = create_message('bannon.c.35@berkeley.edu', 
+                             'bannon.c.35@gmail.com', 
+                             'bannon.c.35@berkeley.edu', 
+                             'Email test pt 2', 
+                             'This is the body of the email')
+    
+    send_message(message)
+
+
+
+# def test():
+#     message = MIMEText('This is the body of the email')
+#     message['to'] = 'bannon.c.35@gmail.com'
+#     message['subject'] = 'Email test pt 2'
+#     message['from'] = 'bannon.c.35@berkeley.edu'
+#     message['Cc'] = 'bannon.c.35@berkeley.edu'
+#     create_message = {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
+
+#     try:
+#         message = (service.users().messages().send(userId="me", body=create_message).execute())
+#         print(F'sent message to {message} Message Id: {message["id"]}')
+#     except HTTPError as error:
+#         print(F'An error occurred: {error}')
+#         message = None
