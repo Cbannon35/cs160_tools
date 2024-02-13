3fb239e3f593d55fe37dd183cec6ee799ca4563c
Christopher Bannon <bannon.c.35@berkeley.edu>
2024-02-13 00:49:10 -0800

    some more changes... this is pre group # tho so will need more tweaks in future

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import sys
 import os
 from dotenv import load_dotenv
-from openai import OpenAI
-client = client = OpenAI()
+import subprocess
 
 load_dotenv()
 
@@ -10,7 +9,6 @@ load_dotenv()
 # Please pass in the row from spreadsheet into the command line
 # Please put the assignment in quotes if it has spaces
 def main():
-    # print_args()
 
     date = sys.argv[1]
     time = sys.argv[2]
@@ -22,67 +20,62 @@ def main():
     extension_days = sys.argv[8]
     extension_date = sys.argv[9]
     dsp = sys.argv[10]
-    reason = sys.argv[11]
+    # reason = sys.argv[11] not needed
 
     flag = True
+    special = True
 
-    while True:
-        user_input = input("Accept Extension? y or n ")
-        print('')
-        if user_input.lower() == 'y':
-            flag = True
-            break
-        elif user_input.lower() == 'n':
-            flag = False
-            break
-        else:
-            print("Please enter 'y' or 'n'")
-            print('')
+    # auto accept dsp
+    if dsp != "Yes":
+        while True:
+            user_input = input("Accept Extension? y or n ")
+            if user_input.lower() == 'y':
+                flag = True
+                break
+            elif user_input.lower() == 'n':
+                flag = False
+                break
+            else:
+                print("Please enter 'y' or 'n'")
+                print('')
+        
+        while int(extension_days) > 3:
+            user_input = input("Grant greater than 3 days? y or n ")
+            if user_input.lower() == 'y':
+                special = True # user has a special reason to have more than 3 days
+                break
+            elif user_input.lower() == 'n':
+                special = False # user does not have a special reason
+                break
+            else:
+                print("Please enter 'y' or 'n'")
+                print('')
     
-    email_body = read_email(flag)
+    email_body = read_email(flag, special)
     email_body = email_body.replace('~NAME~', first_name)
     email_body = email_body.replace('~ASSIGNMENT~', assignment)
     email_body = email_body.replace('~DATE~', extension_date)
     email_body = email_body.replace('~DUE_DATE~', due_date)
+    email_body = email_body.replace('~DAYS~', extension_days)
 
-    print("SUBJECT: ")
-    print("Extension Request for " + ask_gpt() + assignment)
+    subject = "CS160 Extension Request"
+    cc = os.getenv('CC_EMAIL')
+    send_email(subject, email, cc, email_body)
 
-    print("\nTO: ")
-    print(email)
+def send_email(subject, to, cc, body):
+    '''Opens the apple mail app with fields filled out'''
+    mailto_url = f"mailto:{to}?cc={cc}&subject={subject}&body={body}"
 
-    print("\nCC: ")
-    print(os.getenv('CC_EMAIL'))
+    # Open Apple Mail with pre-filled fields
+    subprocess.run(['open', mailto_url])
 
-    print("\nBODY:\n")
-    print(email_body)
-
-
-def ask_gpt() -> str:
-    used = ""
-    with open('used.txt', 'r') as f:
-        used = f.read()
-
-    prompt="Please only respond with 1 singular word. Do not elaborate. No extra text. Simply respond with a mis-spelling of the word 'extension'. Avoid using duplicates here: {used}",
-    completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": prompt},
-    ]
-    )
-
-    # Send a request to GPT-3 with the provided prompt
-    response = completion.choices[0].message
-    used += ", {response}"
-    with open('used.txt', 'w') as f:
-        f.write(used)
-
-    # Parse and return the response text
-    return response
-    
-
-def read_email(flag: bool) -> str:
-    '''True --> Accept | False --> Reject'''
+def read_email(flag: bool, special: bool) -> str:
+    ''' Flag:    True --> Accept | False --> Reject
+        Special: True --> Student doesn't have DSP and asked for > 3 days without 'extreme' reason
+    '''
+    if not special:
+        with open('email_accept_but_3_days.txt', 'r') as f:
+            return f.read()
     if flag:
         with open('email_accept.txt', 'r') as f:
             return f.read()
@@ -90,6 +83,17 @@ def read_email(flag: bool) -> str:
         with open('email_reject.txt', 'r') as f:
             return f.read()
 
+if __name__ == "__main__":
+    print("GENERATING EMAIL STUFF...\n")
+    main()
+
+
+
+###
+
+# Used for debug
+
+###
 def write_email(email_body: str, subject: str, to: str, cc: str):
     to_write = f"Subject: {subject}\nTo: {to}\nCC: {cc}\n\n{email_body}"
     with open('email.txt', 'w') as f:
@@ -99,7 +103,3 @@ def print_args():
     print("Arguments provided:")
     for i, arg in enumerate(sys.argv[1:], start=1):
         print(f"Argument {i}: {arg}")
-
-if __name__ == "__main__":
-    print("GENERATING EMAIL STUFF...\n")
-    main()
